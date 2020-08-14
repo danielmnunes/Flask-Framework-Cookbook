8915d388a51372af4309eba8eda3b4fe8ad0ef1e
Daniel Nunes <danielmnunes08@gmail.com>
2020-08-13 21:46:43 -0300

    Handling basic configurations

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Flask-Framework-Cookbook
-Flask Framework Cookbook
+# Flask Framework Cookbook
 
 > "Flask is a microframework for Python based on Werkzeug, Jinja 2, and good intentions." - Flask official documentation
--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True # Object level
+app.debug = True # Config leval
+# app.run(debug=True) # named argument
+# $ export or Set FLASK_DEBUG = 1 # Set on an environment variable
+
+
 @app.route('/')
 def index():
     return 'Hello World!!'
